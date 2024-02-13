30ea9331890813354fbdb563489175e81162abad
tyson lind <tysonfloydlind@gmail.com>
2024-02-13 14:45:26 -0700

    had to add using for the tests to properly access the palindromeexercise namespace

--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,13 +1,23 @@
 using System;
 using Xunit;
+using PalindromeExercise;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("banana", false)]
+        [InlineData("civic", true)]
+        [InlineData("Level", true)]
+        public void PalindromeTest(string word, bool expected)
         {
+            Palindrome pali = new Palindrome();
+
+            var actual = pali.IsPalindrome(word);
+
+            Assert.Equal(expected, actual);
 
         }
     }
