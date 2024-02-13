8519d5afcfa3adf982b95f14be2d75cf46a11a4b
tyson lind <tysonfloydlind@gmail.com>
2024-02-13 03:43:57 -0700

    finished exercise

---
+++ PalindromeExercise/PalindromeExercise/Palindrome.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeExercise
+{
+    public class Palindrome
+    {
+        public bool isPalindrome(string str)
+        {
+            string[] strArr = str.Split();
+            string[] strArrRev = strArr.Reverse().ToArray();
+
+            for (int i = 0; i < strArr.Length; i++)
+            {
+                if (strArr[i] != strArrRev[i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
